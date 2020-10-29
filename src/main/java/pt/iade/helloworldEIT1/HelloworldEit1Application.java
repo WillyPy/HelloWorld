a003b4f137c9f798d5b1c42766af439617be5f1f
William Valter Ambrósio Feijó <50035088@iade.pt>
2020-10-29 17:40:38 +0000

    Initial Commit

---
+++ src/main/java/pt/iade/helloworldEIT1/HelloworldEit1Application.java
@@ -0,0 +1,13 @@
+package pt.iade.helloworldEIT1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldEit1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldEit1Application.class, args);
+	}
+
+}
