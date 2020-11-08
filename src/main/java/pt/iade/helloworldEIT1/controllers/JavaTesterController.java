116d4b1745fe227a30ed4687b98d1bba978a5ec5
William Valter Ambrósio Feijó <50035088@iade.pt>
2020-11-08 22:00:53 +0000

    Update

--- src/main/java/pt/iade/helloworldEIT1/controllers/JavaTesterController.java
+++ src/main/java/pt/iade/helloworldEIT1/controllers/JavaTesterController.java
@@ -17,6 +17,8 @@ public class JavaTesterController {
     private boolean footballFan;
     private char color;
     private String auxLabel;
+    private double grades[]={10.5, 12, 14.5};
+    private String ucs[]={"FP","POO","BD"};
 
     @GetMapping(path = "/author", produces= MediaType.APPLICATION_JSON_VALUE)
     public String getAuthor() {
@@ -35,5 +37,45 @@ public class JavaTesterController {
         logger.info("Saying Hello to"+author);
         return "Done by "+name+" with number "+number+". I am "+height+" tall and "+auxLabel;
     }
+
+    @GetMapping(path = "/access/{student}/{covid}",
+    produces= MediaType.APPLICATION_JSON_VALUE)
+    public boolean getGreeting(@PathVariable("student") boolean isStudent, @PathVariable("covid") boolean hasCovid){
+        if(isStudent && !hasCovid){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @GetMapping(path= "/required/{student}/{temperature}/{classType}",
+    produces= MediaType.APPLICATION_JSON_VALUE)
+    public boolean getRequired(@PathVariable("student") boolean isStudent,
+                                @PathVariable("temperature") double hasCovid,
+                                @PathVariable("classType") String type){
+        if(isStudent && type.equals("presential") && (hasCovid >= 34.5 && hasCovid <= 37.5)) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @GetMapping(path= "/evacuation/{fire}/{numberOfCovids}/{powerShutdown}/{comeBackTime}/",
+    produces= MediaType.APPLICATION_JSON_VALUE)
+    public boolean getSignature(@PathVariable("fire") boolean isFire,
+                                @PathVariable("numberOfCovids") int covidNumbers,
+                                @PathVariable("powerShutdown") boolean power,
+                                @PathVariable("comeBackTime") int time){
+        if( isFire || (covidNumbers>5) || (power && time>15 )){
+            return true;
+        }else{
+            return false;
+        }
+    }
     
+  /*  @getMapping(path="/avarage/{}", produces= MediaType.APPLICATION_JSON_VALUE)
+    public double getAvarage(){
+
+    }*/
+
 }
